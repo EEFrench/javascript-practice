db7083b4ab826c62dc489df48f368a41c2745f22
EEFrench <edward.e.french@gmail.com>
2021-06-30 22:57:04 -0700

    feature. Added javascript

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//This is a single line comment
+/*this would considered a multi-line comment
+If you are a Dreamer
+Come in
+If you are a Dreamer, a Wisher, a Liar
+A Hoper, a Prayer, a Magic Bean Buyer
+If you are a Pretender come sit by my fire
+For we have some flax golden tales to spin
+Come in!
+Come in!
+-Shel Silverstein*/
+
+
+
+
+console.log("Call me Mr Ed");
+console.log("I don't really have a favorite food...");
+console.log("But if I was forced to make a choice it would be:")
+console.log("No-Bake Cookies");
