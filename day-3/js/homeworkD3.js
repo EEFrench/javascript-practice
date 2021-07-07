647b27d6a0750342ee7303c330ef8baa0a2bfb6a
EEFrench <edward.e.french@gmail.com>
2021-07-06 22:41:27 -0700

    homework

---
+++ day-3/js/homeworkD3.js
@@ -0,0 +1,16 @@
+// Ask for the user's first name
+
+// Ask for the user's last name
+// Log the user's first name to the console
+// Alert the user's last name
+function  myFunction (){
+    const firstName = prompt("What's Your First Name?");
+    console.log (firstName)
+    const lastName = prompt("What's Your Last Name?");
+    alert(lastName)
+}
+
+// *BONUS* 
+// Ask for the user's birthday
+// Ask the user to confirm their birthday input
+// Alert the user's birthday
