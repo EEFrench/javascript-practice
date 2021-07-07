d47552970ce5e97bf28c66b7de55dd0f5b361243
EEFrench <edward.e.french@gmail.com>
2021-07-06 21:07:08 -0700

    homework

--- day-3/js/indexd3.js
+++ day-3/js/indexd3.js
@@ -1,33 +1,6 @@
-document.title = 'Day 3 Daily';
-
-const h1 = document.querySelector('h1');
-h1.innerText = 'Name Game';
-
-
-const nameInput = document.createElement('input');
-nameInput.type = 'string';
-nameInput.placeholder = 'Enter First Name';
-
-
-const nameButton = document.createElement('button');
-nameButton.innerHTML = 'Try it';
-
-
-
-
-
-
-document.body.appendChild(nameInput);
-document.body.appendChild(nameButton);
-
-/*tipButton.addEventListener('click', function() {
-    console.log('click');
-
-    const total = Number(totalInput.value);
-    const percent = Number(percentageInput.value);
-    
-   
-    console.log(percent / 100 * total);
-    h1.innerText = percent / 100 * total;
-})*/
+function  myFunction (){
+    const firstName = prompt("What's Your First Name?");
+    const firstNameFlip =firstName.split('').reverse().join('')
+    alert(firstNameFlip)
+}
 
