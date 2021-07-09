dd237c0276847e470af15dd669f8aec531997250
EEFrench <edward.e.french@gmail.com>
2021-07-08 17:50:50 -0700

    homework

--- day-4/js/daily4.js
+++ day-4/js/daily4.js
@@ -1,4 +1,11 @@
-document.title = 'Daily 4';
+//DAY 4 DAILY CHALLENGE
+// Prompt the user for two different numbers
+// Convert the values to integers
+// Store the two integers as variables
+// Compare the two numbers
+// Alert the larger number
+
+document.title = 'Day 4';
 
 const h1 = document.querySelector('h1');
 h1.innerText = 'Compare Numbers';
@@ -8,14 +15,13 @@ const firstNumber = document.createElement('input');
 firstNumber.type = 'number';
 firstNumber.placeholder = 'First Number';
 
-
 const secondNumber = document.createElement('input');
 secondNumber.type = 'number';
 secondNumber.placeholder = 'Second Number';
 
+
 const compareButton = document.createElement('button');
 compareButton.innerHTML = 'Compare';
-
 
 
 
@@ -23,14 +29,29 @@ document.body.appendChild(firstNumber);
 document.body.appendChild(secondNumber);
 document.body.appendChild(compareButton);
 
-compareButton.addEventListener('click', function() {
-    console.log('click');
 
-    const total = Number(firstNumber.value);
-    const percent = Number(secondNumber.value);
+
+compareButton.addEventListener('click', function() {
+     //console.log('click');
+     compare();
     
-   
-    console.log(percent / 100 * total);
-    h1.innerText = percent / 100 * total;
 })
+
+const number1 = Number(firstNumber.value);
+const number2 = Number(secondNumber.value);
+
+function compare(){
+    if (number1 > number2){
+        alert(number1 = " is larger then " + number2);
+    }
+    else if (number2 > number1) {
+        alert(number2 + " is larger then " + number1);
+    }
+    else if (number1 == number2){
+        alert(number1 + " is equal to " + number2)
+    }
+}
+
+
+
 
