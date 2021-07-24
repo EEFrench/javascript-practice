d2f50ec4a7a7eccba6b0a867c8dc1b959462344a
EEFrench <edward.e.french@gmail.com>
2021-07-23 21:47:44 -0700

    finished Daily for Day 8

--- day-8/js/daily8.js
+++ day-8/js/daily8.js
@@ -10,25 +10,40 @@ const dog = {
     speak: function (){
         alert ("VOOOFFF!")
     },
+    //PROPERTY "CARROT" ASSIGNED FUNCTION TO MAKE DOG NOD
     carrot: function(){
-        alert ("*Watson nodds & paws at the air*");
+        alert ("*Nods & paws at the air*");
     }
 };
+//ADD INFO TO DOCUMENT
+
 
 //CREATE BUTTON TO MAKE WATSON BARK
 const WatsonSpeak = document.createElement("button");
 WatsonSpeak.innerHTML = "Speak, Watson... Speak!";
 
-//CREATE BUTTON TO MAKE WATSON BARK
+//CREATE BUTTON TO MAKE WATSON NOD FOR CARROT
 const wantCarrot = document.createElement("button");
 wantCarrot.innerHTML = "Does Watson want a carrot?";
 
-//ADD BUTTON TO DOC
+//ADD BUTTONS TO DOC
 document.body.appendChild(WatsonSpeak);
 document.body.appendChild(wantCarrot);
 //CALL ON SPEAK FUNCTION
-WatsonSpeak.addEventListener('click', dog.speak, dog.speak, dog.speak)
+WatsonSpeak.addEventListener('click', dog.speak)
+//CALL ON CARROT FUNCTION
 wantCarrot.addEventListener('click', dog.carrot)
+
+//ADD DETAILS
+const dogName = document.getElementById('name');
+const dogBreed = document.getElementById('breed');
+const dogFur = document.getElementById('color');
+const dogDisposition = document.getElementById('disposition')
+
+
+//INSERT DETAILS INTO DOC
+dogName.innerText = "My dog's name is " + dog.name;
+dogBreed.innerText = "He's a " + dog.age + " year old " + dog.disposition + " " + dog.breed + " with " + dog.color + " fur."
 
 
 
