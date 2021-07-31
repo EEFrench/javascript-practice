3d58d44a49b5dbad30674e999ae2baed79736268
EEFrench <edward.e.french@gmail.com>
2021-07-30 23:16:41 -0700

    Update homework

--- day-10/js/top-down-game.js
+++ day-10/js/top-down-game.js
@@ -1,12 +1,122 @@
-const second = 1000;
-
-const mytime = setInterval(function() {
-        console.log('It. Is. TIME...')
-    }, 3 * second);
-
-setTimeout ( ()=> {
-    clearInterval(mytime);
-}, 18 * second);
+// erase daily-challenge
+document.body.innerHTML = '';
 
 
+// --- Variables ---
+const playerElement = document.createElement('img');
+const obstacles = [];
+const screenWidth = document.body.clientWidth;
 
+let jumpValue = 0;
+let speed = -10;
+let jumpDirection = 0;
+let playerX = parseInt(playerElement.style.left);
+// --- End Variables ---
+
+// --- Instantiate Variables ---
+document.body.appendChild(playerElement);
+
+obstacles.push(createObstacle(300), createObstacle(600));
+
+playerElement.style.height = '100px';
+playerElement.style.width = '100px';
+playerElement.src = 'https://cdn.dribbble.com/users/19482/screenshots/2466218/frog-dribbble.gif';
+playerElement.style.position = 'absolute';
+playerElement.style.left = '200px';
+playerElement.style.top = '200px';
+// --- End Instantiate Variables ---
+
+// --- Functions ---
+function createObstacle(leftValue) {
+    const obstacle = document.createElement('div');
+    document.body.appendChild(obstacle);
+    obstacle.style.backgroundColor = 'red';
+    obstacle.style.position = 'absolute';
+    obstacle.style.width = '20px';
+    obstacle.style.height = '20px';
+    obstacle.style.top = '275px';
+    obstacle.style.left = leftValue + 'px';
+    obstacle.location = leftValue;
+    return obstacle;
+}
+
+
+function move(direction, distance) {
+    if (speed < 0 ) {
+        playerElement.style.transform = 'scaleX(-1)';
+    } else {
+        playerElement.style.transform = 'scaleX(1)';
+    }
+    playerX = parseInt(playerElement.style[direction]);
+    playerElement.style[direction] = playerX + distance + 'px';
+}
+
+
+function getLeftValue() {
+    return parseInt(playerElement.style.left)
+}
+
+
+function getTopValue() {
+    return parseInt(playerElement.style.top)
+}
+
+
+function jump() {
+    if (getTopValue() === 200) {
+        jumpSpeed = -25;
+        let myInterval = setInterval(() => {
+            move('top', jumpSpeed);
+            if (getTopValue() <  100) {
+                jumpSpeed = 25;
+            }
+            if (getTopValue() >=  200) {
+                jumpSpeed = 0;
+                playerElement.style.top = '200px';
+                clearInterval(myInterval);
+            }
+        }, 100)
+    }
+}
+
+
+function hitDetection() {
+    obstacles.forEach((obstacle) => {
+        if (speed > 0 ){
+            if (getLeftValue() + 70 === obstacle.location && getTopValue() > 180) {
+                alert('you hit the obstacle')
+            }
+        }
+        else if (getLeftValue() === obstacle.location && getTopValue() > 180) {
+                alert('you hit the obstacle')
+            }
+    })
+}
+
+// --- End Functions ---
+
+// -- auto movement --
+setInterval(function() {
+    if (speed < 0) {
+        move('left', -10);
+        if (getLeftValue() < 0) {
+            speed = 10;
+        }
+    }
+    else {     
+        move('left', 10);
+        if (getLeftValue() > screenWidth - 100) {
+            speed = -10;
+        }
+    }
+    hitDetection();
+}, 100)
+// -- end auto movement --
+
+// -- enable jump button --
+document.addEventListener('keydown', function(event){
+    if (event.code == 'Space'){
+            jump();
+    }
+});
+// -- end enable jump button --     
--- day-9/js/daily9.js
+++ day-9/js/daily9.js
@@ -1,40 +1,76 @@
-const form =document.createElement('form');
+const form = document.createElement('form');
 document.body.appendChild(form);
-// const emailInput =document.createElement('input');
-// form.appendChild(emailInput);
-// emailInput.placeholder = "Enter Username";
-
-const usernameInput = addInput('input', 'enter Name');
-const emailInput = addInput('input', 'Enter Email')
 
 
-function addInput (type, placeholder){
+const usernameInput = addInput('input', 'Enter Usernae');
+const emailInput = addInput('input', 'Enter Email');
+const phoneNumberInput = addInput('input', 'Enter Address');
+const submitButton = addButton('submit');
+
+const userInfoElement = document.createElement('div');
+const userNameElement = document.createElement('h1');
+const phoneNumberElement = document.createElement('p');
+const emailElement = document.createElement('p');
+const restartButton = document.createElement('button');
+document.body.appendChild(userInfoElement);
+
+restartButton.innerHTML = 'Restart';
+
+userInfoElement.appendChild(userNameElement);
+userInfoElement.appendChild(phoneNumberElement);
+userInfoElement.appendChild(emailElement);
+userInfoElement.appendChild(restartButton);
+userInfoElement.style.display = 'none';
+
+restartButton.addEventListener('click', function() {
+    toggleFormUserInfo();
+    setInputValues('', '', '');
+})
+
+
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = form[0].value;
+    const email = form[1].value;
+    const phoneNumber = form[2].value;
+    displayUserInfo(username, email, phoneNumber);
+    toggleFormUserInfo();
+})
+
+function addInput(type, placeholder) {
     const input = document.createElement(type);
     form.appendChild(input);
-    emailInput.placeholder = "Enter Username";
+    input.placeholder = placeholder;
 }
 
-function addButton (text){
-    const input = document.createElement(button);
-    form.appendChild(input);
-    emailInput.innerHtml = text;
+function addButton(text){
+    const button = document.createElement('button');
+    form.appendChild(button);
+    button.innerHTML = text;
+    return button;
 }
 
+function displayUserInfo(username, email, phoneNumber) {
+    userNameElement.innerText = username;
+    emailElement.innerText = email;
+    phoneNumberElement.innerText = phoneNumber;
+}
 
+function setInputValues(username, email, phoneNumber) {
+    form[0].value = username;
+    form[1].value = email;
+    form[2].value = phoneNumber;
+}
 
+function toggleFormUserInfo() {
 
-
-
-
-
-// const firstNumber = document.createElement("input");
-// firstNumber.type = "number";
-// firstNumber.placeholder = "First Number";
-
-// const secondNumber = document.createElement("input");
-// secondNumber.type = "number";
-// secondNumber.placeholder = "Second Number";
-
-// document.body.appendChild(firstNumber);
-// document.body.appendChild(secondNumber);
-// document.body.appendChild(compareButton);
+    if (form.style.display === 'none') {
+        form.style.display = 'block';
+        userInfoElement.style.display = 'none';
+    } else {
+        form.style.display = 'none';
+        userInfoElement.style.display = 'block';
+    }
+   
+}
